12738cc1afe26c0b90c03f65b033ce8caf3c3637
richard caleel <swamiprobably@richards-MacBook-Pro.local>
2017-01-26 15:11:43 -0800

    sorting problem commit

---
+++ my_sort.py
@@ -0,0 +1,22 @@
+list_one = [67, 45, 2, 13, 1, 998]
+list_two = [89, 23, 33, 45, 10, 12, 45, 45, 45]
+
+def my_sort(list):
+    new_list = []
+    new_list.append(list[0])
+    for i in range (1, len(list)):
+        for j in range (0, len(new_list)):
+            if (list[i] < new_list[j]):
+                if (j == 0):
+                    new_list.insert(0, list[i])
+                    break
+                else:
+                    new_list.insert(j, list[i])
+                    break
+
+    print new_list
+
+
+
+my_sort(list_one)
+my_sort(list_two)
