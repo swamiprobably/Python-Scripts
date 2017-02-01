29c758032d41b5f0b5b51e0bbf3da17e05b21bd5
richard caleel <swamiprobably@richards-MacBook-Pro.local>
2017-01-31 20:34:47 -0800

    Adjusted syntax

--- file_move2.py
+++ file_move2.py
@@ -6,8 +6,8 @@ source = '/Users/swamiprobably/Desktop/Folder_A'
 destination = '/Users/swamiprobably/Desktop/Folder_B'
 
 for files in os.listdir(source):                #Loop through dir
-    if files.endswith('.txt'):                  #Find all .txt files
+    if files.endswith('.txt')                   #Find all .txt files
         path = os.path.join(source, files)      #Save file path
-        print(path)                             #Print path for moved file
+        print('File path: {}').format(path)     #Print path for moved file
         shutil.move(path, destination)          #Move file to new destination
     
